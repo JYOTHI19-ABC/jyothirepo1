1ecc8c167953dc768264aae440339bbe39f20585
jyothihm <jyothi7338391@gmail.com>
2020-07-08 19:14:57 +0530

    WEB

---
+++ web-app1/src/main/java/com/web/LoginServlet.java
@@ -0,0 +1,56 @@
+package com.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+//@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name=request.getParameter("USERNAME");
+		String pwd=request.getParameter("PASSWORD");
+		
+		System.out.println("USERNAME:" + name);
+		System.out.println("PASSWORD:" + pwd);
+		
+		PrintWriter writer = response.getWriter();
+		String htmlresponse="<html>";
+		htmlresponse +="<h2> Your UserName Is:" +name+ "<br/>";
+	    htmlresponse +=" Your PassWord Is:" +pwd+ "</h2>";
+	    htmlresponse += "</html>";
+	    
+	    writer.println(htmlresponse);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
